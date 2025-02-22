51ddd5d098e487f5a5edde05adadc33bb4e65329
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2025-02-22 14:30:56 +0500

    fixes log error

--- dags/practice-dag.py
+++ dags/practice-dag.py
@@ -10,21 +10,21 @@ def extract(**kwargs):
     ti = kwargs['ti']
     response = requests.get("https://monark.com.pk/products/mnk24-07wt?variant=40177525784705")
     ti.xcom_push(value=response.text , key="html_response" )
-    ti.log.info("html response : " ,response.text)
+    ti.log.info("html response length: %s" ,len(response.text))
 
 def transform(**kwargs):
     
     ti = kwargs['ti']
-    html_reponse = ti.xcom_pull(key="html_response")
-    ti.log.info("html response : " , len(html_reponse))
+    html_reponse = ti.xcom_pull(key="html_response" , task_ids="extract_task" )
+    ti.log.info("html response length : %s" , len(html_reponse))
     
-    html = BeautifulSoup(html_reponse)
-    price_div = html.find('.ts4-price')
+    html = BeautifulSoup(html_reponse , features='lxml')
+    price_div = html.find('div', class_='t4s-product-price')
     ti.log.info("price div : " , price_div)
     
     
 with DAG(
-    'price_wice_dag',
+    'price_wice_dag_v3',
     default_args={ 'retries' : 2 },
     schedule_interval="* * * * *",
     start_date= pendulum.datetime(2025, 2, 19),
